c85f7bd072f0e2fa6bf4b822c6c66674882c2323
mauroxcf <maurox03@gmail.com>
2021-04-28 22:49:08 -0500

    setting the models of the database and the mode admin to manipulate the database

--- assessment/app/admin.py
+++ assessment/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+# here we can import our models of the database
+from .models import User, Order, payment
+
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Order)
+admin.site.register(payment)
--- assessment/app/models.py
+++ assessment/app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Users(models.Model):
+class User(models.Model):
     idUser = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
@@ -9,21 +9,26 @@ class Users(models.Model):
     email = models.CharField(max_length=60)
     company = models.CharField(max_length=45)
 
+    def __str__(self):
+        return self.name + ' ' + self.last_name
 class Order(models.Model):
     idOrder = models.IntegerField(primary_key=True)
     date = models.DateField()
-    total = models.FloatField()
     subtotal = models.FloatField()
     taxes = models.FloatField()
+    total = models.FloatField()
     paid = models.FloatField()
-    id_user = models.ForeignKey(Users, on_delete = models.CASCADE)
+    id_user = models.ForeignKey(User, on_delete = models.CASCADE)
     address = models.CharField(max_length=45)
     city = models.CharField(max_length=45)
     state = models.CharField(max_length=45)
     country = models.CharField(max_length=45)
-    cost = models.FloatField()
+    cost_of_shipping = models.FloatField()
 
-class payments(models.Model):
+    def __str__(self):
+        return self.idOrder
+
+class payment(models.Model):
     idpayment = models.IntegerField(primary_key=True)
     types = models.CharField(max_length=45)
     date = models.DateField()
@@ -31,3 +36,6 @@ class payments(models.Model):
     total = models.FloatField()
     status = models.CharField(max_length=45)
     id_order = models.ForeignKey(Order, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.idpayment
